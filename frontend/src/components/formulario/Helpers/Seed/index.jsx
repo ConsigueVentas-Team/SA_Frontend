2fd1c4d1ca8337385008f5323fe82ff1b510d6aa
Jacn_159 <jcasma37@gmail.com>
2023-09-15 09:57:14 -0500

    añadiendo spinner en login y tablas

--- frontend/src/components/formulario/Helpers/Seed/index.jsx
+++ frontend/src/components/formulario/Helpers/Seed/index.jsx
@@ -1,16 +1,20 @@
 import { Breadcrumbs } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
-
+import MapsHomeWorkOutlinedIcon from "@mui/icons-material/MapsHomeWorkOutlined";
 export default function ActiveLastBreadcrumb({ actual }) {
   return (
     <Breadcrumbs color="#BBBDBC">
-      <Link to="/empresa" className="font-medium">
+      <Link
+        to="/empresa"
+        className=" gap-2 flex items-center text-base font-medium text-gray-500 hover:text-white uppercase"
+      >
+        <MapsHomeWorkOutlinedIcon />
         Empresa
       </Link>
       <Link
         to={`/empresa/${actual}`}
-        className="font-sans capitalize font-medium"
+        className="flex items-center text-white font-medium uppercase"
       >
         {actual}
       </Link>
