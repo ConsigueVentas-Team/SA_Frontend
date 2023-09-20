c64b3bfc1e8ca280bd6b9372764c2fde96538aa1
cristianvasquezc <vasquezcristian140@gmail.com>
2023-09-20 12:45:50 -0500

    subiendo cambios en 404

--- frontend/src/pages/Error404.jsx
+++ frontend/src/pages/Error404.jsx
@@ -2,22 +2,9 @@ import { Link } from "react-router-dom";
 
 export const Error404 = () => {
   return (
-    // <section className="flex items-center h-full p-16 bg-cv-secondary">
-    //   <div className="container flex flex-col items-center ">
-    //     <div className="flex flex-col gap-6 max-w-xl text-center">
-    //       <h2 className="font-extrabold text-9xl text-cv-primary">
-    //         <span className="sr-only">Error</span>404
-    //       </h2>
-    //       <p className="text-lg md:text-2xl text-slate-300">Lo sentimos, no pudimos encontrar esta página.</p>
-    //       <Link to="/" className="px-8 py-4 text-xl font-semibold rounded bg-cv-cyan text-cv-primary hover:text-gray-200">Back to homepage</Link>
-    //     </div>
-    //   </div>
-    // </section>
-
-
-    <section className="h-full w-full flex flex-col justify-center items-center bg-cv-secondary">
-      <h1 className="text-9xl font-extrabold text-white tracking-widest">404</h1>
-      <div className="bg-cv-cyan text-cv-primary px-2 text-sm font-semibold rounded rotate-12 absolute">
+    <section className="flex flex-col items-center justify-center w-full h-full bg-cv-secondary">
+      <h1 className="font-extrabold tracking-widest text-white text-9xl">404</h1>
+      <div className="absolute px-2 text-sm font-semibold rounded bg-cv-cyan text-cv-primary rotate-12">
         Pagina no encontrada
       </div>
       <button className="mt-5">
@@ -28,7 +15,7 @@ export const Error404 = () => {
             className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-cv-cyan group-hover:translate-y-0 group-hover:translate-x-0"
           ></span>
 
-            <Link to="/" className="relative block px-8 py-3 bg-cv-primary border border-current">Go Home</Link>
+          <Link to="/" className="relative block px-8 py-3 border border-current bg-cv-primary">Go Home</Link>
         </a>
       </button>
     </section>
