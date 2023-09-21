23ba948219bf8da04d22323a4e058c7555e9b47f
cristianvasquezc <vasquezcristian140@gmail.com>
2023-09-21 11:38:48 -0500

    Corrigiendo Error Modal IMagen Falta

--- frontend/src/components/asistencias/Asistencias/ModalImagen.jsx
+++ frontend/src/components/asistencias/Asistencias/ModalImagen.jsx
@@ -5,20 +5,20 @@ export const ModalImagen = ({ image, closeImageModal }) => {
     return (
         <div>
             <div
-                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none p-2"
+                className="fixed inset-0 z-50 flex items-center justify-center p-2 overflow-x-hidden overflow-y-auto outline-none focus:outline-none"
             >
-                <div className="relative w-full my-6 mx-auto max-w-3xl p-1 rounded-lg ">
+                <div className="relative w-full max-w-3xl p-1 mx-auto my-6 rounded-lg ">
 
-                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                    <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
 
                         <div className="flex items-center justify-center p-2.5 md:p-5 border-b border-solid border-slate-200 rounded-t text-black">
-                            <h3 className="text-xl md:text-3xl font-semibold">
+                            <h3 className="text-xl font-semibold md:text-3xl">
                                 Fotografías de asistencia
                             </h3>
                         </div>
                         {image && (
                             <div>
-                                <div className='text-center text-cv-primary uppercase'>
+                                <div className='text-center uppercase text-cv-primary'>
                                     {image.attendance === 1 && (
                                         <p className='space-x-2 text-lg font-bold bg-[#24FF00] p-2'>
                                             <span>{image.user.name.split(" ")[0] + ' ' + image.user.surname.split(" ")[0]}</span>
@@ -41,42 +41,42 @@ export const ModalImagen = ({ image, closeImageModal }) => {
                                             <span>{image.user.name.split(" ")[0] + ' ' + image.user.surname.split(" ")[0]}</span>
                                             <span>Justifico por Falta</span>
                                         </p>
-                                    ) : image.delay === 0 && image.attendance === 0 && image.justification === 0 (
+                                    ) : image.delay === 0 && image.attendance === 0 && image.justification === 0 && (
                                         <p className='space-x-2 text-lg font-bold text-white bg-[#FF0000] p-2'>
                                             <span>{image.user.name.split(" ")[0] + ' ' + image.user.surname.split(" ")[0]}</span>
                                             <span>Falto</span>
                                         </p>
                                     )}
                                 </div>
-                                <div className="relative p-3 md:p-6 flex-auto">
-                                    <div className='flex flex-col md:flex-row items-center justify-between space-y-2 md:space-x-2 md:space-y-0'>
-                                        <div className='w-full flex flex-col items-center justify-center space-y-2 text-center'>
-                                            <h4 className='font-semibold text-lg'>Fotografía de Entrada</h4>
+                                <div className="relative flex-auto p-3 md:p-6">
+                                    <div className='flex flex-col items-center justify-between space-y-2 md:flex-row md:space-x-2 md:space-y-0'>
+                                        <div className='flex flex-col items-center justify-center w-full space-y-2 text-center'>
+                                            <h4 className='text-lg font-semibold'>Fotografía de Entrada</h4>
                                             {image.admission_image ? (
-                                                <img className='rounded-lg w-4/5 md:w-full border' src={import.meta.env.VITE_BACKEND_SERVER_URL + '/' + image.admission_image} alt="Fotografía de entrada" />
+                                                <img className='w-4/5 border rounded-lg md:w-full' src={import.meta.env.VITE_BACKEND_SERVER_URL + '/' + image.admission_image} alt="Fotografía de entrada" />
                                             ) : (
-                                                <div className='rounded-lg w-4/5 md:w-full border'>
+                                                <div className='w-4/5 border rounded-lg md:w-full'>
                                                     <DefaultImage />
                                                 </div>
                                             )}
                                             {(image.attendance === 1 || image.delay === 1) && (
-                                                <p className='text-lg font-semibold text-cv-primary space-x-3'>
+                                                <p className='space-x-3 text-lg font-semibold text-cv-primary'>
                                                     <span>Hora de Entrada:</span>
                                                     <span>{image.admission_time ? image.admission_time : 'No registrada'}</span>
                                                 </p>
                                             )}
                                         </div>
-                                        <div className='w-full flex flex-col items-center justify-center space-y-2 text-center'>
-                                            <h4 className='font-semibold text-lg'>Fotografía de Salida</h4>
+                                        <div className='flex flex-col items-center justify-center w-full space-y-2 text-center'>
+                                            <h4 className='text-lg font-semibold'>Fotografía de Salida</h4>
                                             {image.departure_image ? (
-                                                <img className='rounded-lg w-4/5 md:w-full border' src={import.meta.env.VITE_BACKEND_SERVER_URL + '/' + image.departure_image} alt="Fotografía de salida" />
+                                                <img className='w-4/5 border rounded-lg md:w-full' src={import.meta.env.VITE_BACKEND_SERVER_URL + '/' + image.departure_image} alt="Fotografía de salida" />
                                             ) : (
-                                                <div className='rounded-lg w-4/5 md:w-full border'>
-                                                    <DefaultImage/>
+                                                <div className='w-4/5 border rounded-lg md:w-full'>
+                                                    <DefaultImage />
                                                 </div>
                                             )}
                                             {(image.attendance === 1 || image.delay === 1) && (
-                                                <p className='text-lg font-semibold text-cv-primary space-x-3'>
+                                                <p className='space-x-3 text-lg font-semibold text-cv-primary'>
                                                     <span>Hora de Salida:</span>
                                                     <span>{image.departure_time ? image.departure_time : 'No registrada'}</span>
                                                 </p>
@@ -86,9 +86,9 @@ export const ModalImagen = ({ image, closeImageModal }) => {
                                 </div>
                             </div>
                         )}
-                        <div className="flex items-center justify-end p-3 border-t border-solid border-slate-200 rounded-b">
+                        <div className="flex items-center justify-end p-3 border-t border-solid rounded-b border-slate-200">
                             <button
-                                className="py-2 px-4 rounded-md text-white bg-cv-primary flex items-center justify-center text-xl uppercase active:scale-95 ease-in-out duration-300"
+                                className="flex items-center justify-center px-4 py-2 text-xl text-white uppercase duration-300 ease-in-out rounded-md bg-cv-primary active:scale-95"
                                 type="button"
                                 onClick={closeImageModal}
                             >Cerrar</button>
@@ -96,12 +96,12 @@ export const ModalImagen = ({ image, closeImageModal }) => {
                     </div>
                 </div>
             </div>
-            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
         </div>
     );
 };
 
 ModalImagen.propTypes = {
-	image: PropTypes.array.isRequired,
+    image: PropTypes.array.isRequired,
     closeImageModal: PropTypes.func.isRequired
 };
