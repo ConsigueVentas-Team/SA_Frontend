a0b8997a0077780124adc8870b2f1e4f5bd550b2
Sebastián A. Mendoza Banda <sebastian.a.mendoza.banda@gmail.com>
2023-09-22 11:58:01 -0500

    Implementacion de llamado de API para tabla

--- frontend/src/components/evaluaciones/Evaluador/BasicTable.jsx
+++ frontend/src/components/evaluaciones/Evaluador/BasicTable.jsx
@@ -1,28 +1,66 @@
 import { flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table'
+import { AES, enc } from "crypto-js";
 import FirstPageIcon from '@mui/icons-material/FirstPage';
 import LastPageIcon from '@mui/icons-material/LastPage';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import EditIcon from '@mui/icons-material/Edit';
-import { useState } from "react";
-import fakeData from './fakeData.json'
-import { useMemo } from 'react'
+import { useState, useMemo, useEffect } from "react";
 import SearchBar from './SearchBar'
 
 
 export default function BasicTable() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [filters, setFilters] = useState({
+    name: "",
+    shift: "",
+    department: "",
+    core: "",
+    position: "",
+  });
 
+  const tokenD = AES.decrypt(
+    localStorage.getItem("token"),
+    import.meta.env.VITE_TOKEN_KEY
+  );
+  const token = tokenD.toString(enc.Utf8);
 
-  // [{
-  //   "name": "Pru Ather",
-  //   "email": "pather0@hostgator.com",
-  //   "dni": "49708012",
-  //   "departament": "Product Management",
-  //   "estado": null,
-  //   "acciones": null
-  // }
+  const obtenerUsuarios = useMemo(() => async (page) => {
+    setIsLoading(true);
+    try {
+      const url = new URL(import.meta.env.VITE_API_URL + "/users");
 
-  //modal
+      url.searchParams.append("page", page);
+
+      for (const [key, value] of Object.entries(filters)) {
+        if (value) url.searchParams.append(key, value);
+      }
+
+      const response = await fetch(url, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        setUsers(data.data);
+        setIsLoading(false);
+      } else {
+        console.error("Error al obtener los usuarios:", data.error);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.error("Error al obtener los usuarios:", error);
+      setIsLoading(false);
+    }
+  }, [filters, token]);
+
+  useEffect(() => {
+    obtenerUsuarios(0);
+  }, [obtenerUsuarios]);
   function Modal({ isOpen, onClose, data }) {
     if (!isOpen) return null;
 
@@ -37,7 +75,7 @@ export default function BasicTable() {
               <h2 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">Datos del colaborador</h2>
               <div className="mt-2">
                 <p className="text-sm text-gray-500">
-                  ID: {data.name}
+                  ID: {data.name}+{data.surname}
                 </p>
                 <p className="text-sm text-gray-500">
                   Email: {data.email}
@@ -61,44 +99,56 @@ export default function BasicTable() {
     );
   }
 
+  const [globalFilter, setGlobalFilter] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalData, setModalData] = useState(null);
-
-  const data = useMemo(() => fakeData, [])
-
-  const [globalFilter, setGlobalFilter] = useState('')
-
-
 
   function handleButtonClick(data) {
     setModalData(data);
     setIsModalOpen(true);
   }
 
+  const data = useMemo(() => users, [users]);
+
   const columns = [
     {
       header: 'Nombre',
-      accessorKey: 'name'
-    }, {
+      accessorKey: 'name',
+      cell: ({ row }) => (
+        <span>
+          {row.original.name} {row.original.surname}
+        </span>
+      ),
+    },
+    {
       header: 'Correo',
-      accessorKey: 'email'
+      accessorKey: 'email',
     },
     {
       header: 'DNI',
-      accessorKey: 'dni'
+      accessorKey: 'dni',
     },
-
-    // {
-    //   header: 'Rol',
-    //   accessorKey: 'rol'
-    // }
+    {
+      header: 'Rol',
+      accessorKey: 'rol',
+      cell: ({ row }) => (
+        <span>
+          {row.original.roles[0].name}
+        </span>),
+    },
     {
       header: 'Departamento',
-      accessorKey: 'departament'
-    }, {
+      accessorKey: 'core', cell: ({ row }) => (
+        <span>
+          {row.original.position[0].core.department.name}
+        </span>
+      ),
+    },
+    {
       header: 'Estado',
-      accessorKey: 'estado'
-    }, {
+      accessorKey: 'estado',
+    },
+    {
       header: 'Acciones',
       accessorKey: 'acciones',
       cell: ({ row }) => (
@@ -109,7 +159,7 @@ export default function BasicTable() {
         </button>
       ),
     },
-  ]
+  ];
 
   const table = useReactTable({
     data,
@@ -122,11 +172,11 @@ export default function BasicTable() {
     getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
 
-  })
+  });
+
 
 
   return (
-
     <div className='p-2 max-w-5xl- mx-auto '>
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} data={modalData} />
       <div className=' justify-between mb-2'>
@@ -136,33 +186,33 @@ export default function BasicTable() {
           className=" rounded-md border border-solid border-cv-primary bg-transparent p-2  text-cv-cyan outline-none "
         />
       </div>
-
-      <div className='w-full bg-[#0e161b] shadow-md  rounded-t overflow-hidden'>
-        <table className="w-full text-sm text-center text-white">
-          {table.getHeaderGroups().map(headerGroup => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => <th key={header.id} className='px-6 py-4 whitespace-nowrap text-base uppercase'>
-                {flexRender(header.column.columnDef.header, header.getContext())}
-              </th>)}
-            </tr>
-          ))}
-
-          <tbody className='bg-cv-primary'>
-            {table.getRowModel().rows.map((row) => (
-              <tr key={row.id} className="border-b border-cv-secondary">
-                {row.getVisibleCells().map(cell => (
-                  <td key={cell.id} className="px-6 py-4 whitespace-nowrap">
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
+      {isLoading ? (
+        <div className="text-center">Cargando...</div>
+      ) : (
+        <div className='w-full bg-[#0e161b] shadow-md  rounded-t overflow-hidden'>
+          <table className="w-full text-sm text-center text-white">
+            {table.getHeaderGroups().map(headerGroup => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map(header => <th key={header.id} className='px-6 py-4 whitespace-nowrap text-base uppercase'>
+                  {flexRender(header.column.columnDef.header, header.getContext())}
+                </th>)}
               </tr>
             ))}
-          </tbody>
-        </table>
-      </div>
 
-
-
+            <tbody className='bg-cv-primary'>
+              {table.getRowModel().rows.map((row) => (
+                <tr key={row.id} className="border-b border-cv-secondary">
+                  {row.getVisibleCells().map(cell => (
+                    <td key={cell.id} className="px-6 py-4 whitespace-nowrap">
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
       <div className="w-full flex items-center justify-center md:justify-between bg-[#0e161b] rounded-b p-2">
 
         <span className="flex items-center justify-start gap-1 p-3">
