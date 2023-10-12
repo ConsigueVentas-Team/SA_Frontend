c6713d78938e3f2e46069b09f4dd205311bdf38a
JheysonJhair <jhair2003unamba@gmail.com>
2023-10-12 08:36:59 -0500

    Agregando la iteraccion de las tablas con su rol para evaluacion

--- frontend/src/components/evaluaciones/Evaluador/TablaEvaluaciones.jsx
+++ frontend/src/components/evaluaciones/Evaluador/TablaEvaluaciones.jsx
@@ -1,170 +1,120 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import ModalConfirmacion from "./Modals/ModalConfirmacion";
 
-const TablaEvaluaciones = () => {
-    const [numFilas1, setNumFilas1] = useState(0);
-    const [mostrarModal1, setMostrarModal1] = useState(false);
-    const [mostrarEncabezados1, setMostrarEncabezados1] = useState(false);
+const TablaEvaluaciones = ({ rol }) => {
+  const [numFilas, setNumFilas] = useState(0);
+  const [mostrarModal, setMostrarModal] = useState(false);
+  const [mostrarEncabezados, setMostrarEncabezados] = useState(false);
 
-    const [numFilas2, setNumFilas2] = useState(0);
-    const [mostrarModal2, setMostrarModal2] = useState(false);
-    const [mostrarEncabezados2, setMostrarEncabezados2] = useState(false);
+  const obtenerNombreDelMes = (fecha) => {
+    const meses = [
+      "ENERO",
+      "FEBRERO",
+      "MARZO",
+      "ABRIL",
+      "MAYO",
+      "JUNIO",
+      "JULIO",
+      "AGOSTO",
+      "SEPTIEMBRE",
+      "OCTUBRE",
+      "NOVIEMBRE",
+      "DICIEMBRE",
+    ];
+    return meses[fecha.getMonth()];
+  };
 
-    const fechaActual = new Date();
+  const fechaActual = new Date();
+  const mesActual = obtenerNombreDelMes(fechaActual);
 
-    const obtenerNombreDelMes = (fecha) => {
-        const meses = [
-            "ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO",
-            "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"
-        ];
-        return meses[fecha.getMonth()];
-    };
+  const agregarFila = () => {
+    setMostrarModal(true);
+  };
 
-    const mesActual = obtenerNombreDelMes(fechaActual);
+  const confirmarAgregarFila = () => {
+    setNumFilas(numFilas + 1);
+    setMostrarModal(false);
+    if (numFilas === 0) {
+      setMostrarEncabezados(true);
+    }
+  };
 
-    const agregarFila1 = () => {
-        setMostrarModal1(true);
-    };
+  const cancelarAgregarFila = () => {
+    setMostrarModal(false);
+  };
 
-    const confirmarAgregarFila1 = () => {
-        setNumFilas1(numFilas1 + 1);
-        setMostrarModal1(false);
-        if (numFilas1 === 0) {
-            setMostrarEncabezados1(true);
-        }
-    };
+  const tablaClase = "w-full text-sm text-center text-white";
+  const encabezadosClase = "px-6 py-4 whitespace-nowrap text-base uppercase";
+  const filaClase = "border-b border-cv-secondary ";
+  const botonClase = "uppercase";
+  const celdaClase = "px-6 py-4 whitespace-nowrap";
 
-    const cancelarAgregarFila1 = () => {
-        setMostrarModal1(false);
-    };
+  const renderFilasDeHabilidades = (numFilas, numColumnas, mes) => {
+    const filas = [];
+    for (let i = 0; i < numFilas; i++) {
+      const celdas = [];
+      for (let j = 0; j < numColumnas; j++) {
+        celdas.push(<td key={j} className={celdaClase}></td>);
+      }
+      filas.push(
+        <tr key={i} className={filaClase}>
+          <td className={celdaClase}>{mes}</td>
+          {celdas}
+        </tr>
+      );
+    }
+    return filas;
+  };
 
-    const agregarFila2 = () => {
-        setMostrarModal2(true);
-    };
+  return (
+    <div>
+      <ModalConfirmacion
+        isOpen={mostrarModal}
+        onConfirm={confirmarAgregarFila}
+        onClose={cancelarAgregarFila}
+      />
+      <table className={tablaClase}>
+        <thead>
+          <tr>
+            <th
+              colSpan={rol == "Lider Nucleo" ? 5 : 6}
+              className={encabezadosClase}
+            >
+              {rol == "Lider Nucleo"
+                ? "EVALUACIONES LIDER"
+                : "EVALUACIONES COLABORADOR"}
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-cv-primary">
+          {mostrarEncabezados && (
+            <tr className={`${filaClase} bg-[#0e161b]`}>
+              <td className={celdaClase}>MES</td>
+              <td className={celdaClase}>HABILIDAD 1</td>
+              <td className={celdaClase}>HABILIDAD 2</td>
+              {rol === "Colaborador" && (
+                <td className={celdaClase}>HABILIDAD 3</td>
+              )}
+              <td className={celdaClase}>PROMEDIO</td>
+            </tr>
+          )}
 
-    const confirmarAgregarFila2 = () => {
-        setNumFilas2(numFilas2 + 1);
-        setMostrarModal2(false);
-        if (numFilas2 === 0) {
-            setMostrarEncabezados2(true);
-        }
-    };
-
-    const cancelarAgregarFila2 = () => {
-        setMostrarModal2(false);
-    };
-
-    const tablaClase = "w-full text-sm text-center text-white";
-    const encabezadosClase = "px-6 py-4 whitespace-nowrap text-base uppercase";
-    const filaClase = "border-b border-cv-secondary";
-    const botonClase = "uppercase";
-    const celdaClase = "px-6 py-4 whitespace-nowrap";
-
-    const renderTabla1 = () => {
-        return (
-            <div>
-                <ModalConfirmacion
-                    isOpen={mostrarModal1}
-                    onConfirm={confirmarAgregarFila1}
-                    onClose={cancelarAgregarFila1}
-                />
-                <table className={tablaClase}>
-                    <thead>
-                        <tr>
-                            <th colSpan="5" className={encabezadosClase}>
-                                EVALUACIONES COLABORADOR
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody className="bg-cv-primary">
-                        {mostrarEncabezados1 && (
-                            <tr className={filaClase}>
-                                <td className={celdaClase}>MES</td>
-                                <td className={celdaClase}>HABILIDAD 1</td>
-                                <td className={celdaClase}>HABILIDAD 2</td>
-                                <td className={celdaClase}>HABILIDAD 3</td>
-                                <td className={celdaClase}>PROMEDIO</td>
-                            </tr>
-                        )}
-                        {renderFilasDeHabilidades(numFilas1, 4, mesActual)}
-                        <tr className={filaClase}>
-                            <td colSpan="5" className={celdaClase}>
-                                <button className={botonClase} onClick={agregarFila1}>Agregar Evaluación +</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        );
-    };
-
-    const renderTabla2 = () => {
-        return (
-            <div>
-                <ModalConfirmacion
-                    isOpen={mostrarModal2}
-                    onConfirm={confirmarAgregarFila2}
-                    onClose={cancelarAgregarFila2}
-                />
-                <table className={tablaClase}>
-                    <thead>
-                        <tr>
-                            <th colSpan="4" className={encabezadosClase}>
-                                EVALUACIONES LIDER
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody className="bg-cv-primary">
-                        {mostrarEncabezados2 && (
-                            <tr className={filaClase}>
-                                <td className={celdaClase}>MES</td>
-                                <td className={celdaClase}>HABILIDAD 1</td>
-                                <td className={celdaClase}>HABILIDAD 2</td>
-                                <td className={celdaClase}>PROMEDIO</td>
-                            </tr>
-                        )}
-                        {renderFilasDeHabilidades(numFilas2, 3, mesActual)}
-                        <tr className={filaClase}>
-                            <td colSpan="4" className={celdaClase}>
-                                <button className={botonClase} onClick={agregarFila2}>Agregar Evaluación +</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        );
-    };
-
-    const renderFilasDeHabilidades = (numFilas, numColumnas, mes) => {
-        const filas = [];
-        for (let i = 0; i < numFilas; i++) {
-            const celdas = [];
-            for (let j = 0; j < numColumnas; j++) {
-                celdas.push(
-                    <td key={j} className={celdaClase}></td>
-                );
-            }
-            filas.push(
-                <tr key={i} className={filaClase}>
-                    <td className={celdaClase}>{mes}</td>
-                    {celdas}
-                </tr>
-            );
-        }
-        return filas;
-    };
-
-    return (
-        <div>
-            <div className="w-full bg-[#0e161b] shadow-md rounded-t overflow-hidden overflow-x-auto scrollbar">
-                {renderTabla1()}
-            </div>
-
-            <div className="w-full bg-[#0e161b] shadow-md rounded-t overflow-hidden overflow-x-auto scrollbar">
-                {renderTabla2()}
-            </div>
-        </div>
-    );
+          {renderFilasDeHabilidades(
+            numFilas,
+            rol == "Lider Nucleo" ? 3 : 4,
+            mesActual
+          )}
+          <tr className={filaClase}>
+            <td colSpan={rol == "Lider Nucleo" ? 5 : 6} className={celdaClase}>
+              <button className={botonClase} onClick={agregarFila}>
+                Agregar Evaluación +
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default TablaEvaluaciones;
--- frontend/src/pages/views/evaluaciones/GestionEvaluaciones.jsx
+++ frontend/src/pages/views/evaluaciones/GestionEvaluaciones.jsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import Nota from '../../../components/evaluaciones/Evaluacion/Nota';
-import TablaEvaluaciones from '../../../components/evaluaciones/Evaluador/TablaEvaluaciones';
-import { AES, enc } from 'crypto-js';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Nota from "../../../components/evaluaciones/Evaluacion/Nota";
+import TablaEvaluaciones from "../../../components/evaluaciones/Evaluador/TablaEvaluaciones";
+import { AES, enc } from "crypto-js";
 
 export const GestionEvaluaciones = () => {
   const { id, name } = useParams();
 
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [rol, setRol] = useState(null);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -18,10 +19,7 @@ export const GestionEvaluaciones = () => {
 
         const url = new URL(`${apiUrl}/evaluation/list`);
 
-        const tokenD = AES.decrypt(
-          localStorage.getItem("token"),
-          tokenKey
-        );
+        const tokenD = AES.decrypt(localStorage.getItem("token"), tokenKey);
         const token = tokenD.toString(enc.Utf8);
 
         const response = await fetch(url, {
@@ -36,11 +34,9 @@ export const GestionEvaluaciones = () => {
         }
 
         const data = await response.json();
-        
-        if (data && data.length > 0) {
-          const foundUser = data.filter(item => item.user_id === parseInt(id));
 
-          // console.log(foundUser)
+        if (data && data.length > 0) {
+          const foundUser = data.find((item) => item.user_id === parseInt(id));
 
           if (foundUser) {
             setUser(foundUser.user);
@@ -48,7 +44,9 @@ export const GestionEvaluaciones = () => {
             console.error(`No se encontró un usuario con el ID ${id}.`);
           }
         } else {
-          console.error("No se encontraron usuarios en la respuesta de la API.");
+          console.error(
+            "No se encontraron usuarios en la respuesta de la API."
+          );
         }
 
         setIsLoading(false);
@@ -58,35 +56,74 @@ export const GestionEvaluaciones = () => {
       }
     };
 
-   
-
     fetchUser();
   }, [id]);
 
+  // usuarioo
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const apiUrl = import.meta.env.VITE_API_URL;
+        const tokenKey = import.meta.env.VITE_TOKEN_KEY;
+
+        const url = new URL(`${apiUrl}/users/${id}`); 
+
+        const tokenD = AES.decrypt(localStorage.getItem("token"), tokenKey);
+        const token = tokenD.toString(enc.Utf8);
+
+        const response = await fetch(url, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(
+            `Error al obtener datos del usuario: ${response.status}`
+          );
+        }
+
+        const userData = await response.json();
+
+        setRol(userData.usuario.roles[0].name);
+      } catch (error) {
+        console.error(
+          "Error al obtener los datos adicionales del usuario:",
+          error.message
+        );
+      }
+    };
+
+    fetchUserInfo();
+  }, [id]);
+
   return (
-    <div className='flex flex-col gap-4'>
+    <div className="flex flex-col gap-4">
       {isLoading ? (
-        <div className='w-full rounded-lg bg-cv-primary py-4 px-8'>
-          <p className='text-gray-400'>Cargando usuario  ...</p>
+        <div className="w-full rounded-lg bg-cv-primary py-4 px-8">
+          <p className="text-gray-400">Cargando usuario ...</p>
         </div>
       ) : (
-          <div className='w-full rounded-lg bg-cv-primary py-4 px-8'>
-            <div className='flex flex-row justify-between'>
-              <p className='text-gray-400'>Nombre:</p>
-              <p className='text-gray-400'>Nota Final:</p>
-            </div>
-            <div className='flex flex-row justify-between'>
-              <p>{name
-                .toLowerCase()
-                .split('-')
-                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-                .join(' ')}</p>
-              <p>15.5</p>
-            </div>
+        <div className="w-full rounded-lg bg-cv-primary py-4 px-8">
+          <div className="flex flex-row justify-between">
+            <p className="text-gray-400">Nombre:</p>
+            <p className="text-gray-400">Nota Final:</p>
           </div>
+          <div className="flex flex-row justify-between">
+            <p>
+              {name
+                .toLowerCase()
+                .split("-")
+                .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(" ")}
+            </p>
+            <p>15.5</p>
+          </div>
+        </div>
       )}
-  
-      <TablaEvaluaciones/>
+
+      <TablaEvaluaciones rol={rol} />
     </div>
   );
-}
+};
