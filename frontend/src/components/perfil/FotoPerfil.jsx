477335cc67405fc55bbe402df7b706c22e7aa1a2
moisesvi <moisesvargasms@gmail.com>
2023-10-18 10:46:10 -0500

    correción de errores

--- frontend/src/components/perfil/FotoPerfil.jsx
+++ frontend/src/components/perfil/FotoPerfil.jsx
@@ -1,5 +1,9 @@
 import PropTypes from "prop-types";
 export const FotoPerfil = ({ colaborador }) => {
+
+
+  // console.log(colaborador)
+
   return (
     <div className="col-span-1 md:col-span-2 row-span-5 md:col-start-4 bg-cv-primary rounded-2xl p-5 order-1 md:order-2">
       <div className="w-full h-full flex items-center justify-center">
