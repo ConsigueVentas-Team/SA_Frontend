736e2e3c2b202b776190237c782bda62a93e2615
Caleb <reateguicevallosc@gmail.com>
2024-02-01 12:50:43 -0500

    Fix: Image URL on Perfil

--- frontend/src/components/perfil/FotoPerfil.jsx
+++ frontend/src/components/perfil/FotoPerfil.jsx
@@ -1,16 +1,14 @@
 import PropTypes from "prop-types";
 export const FotoPerfil = ({ colaborador }) => {
 
-  // console.log(colaborador)
+  console.log("Data: ",colaborador)
 
   return (
     <div className="col-span-1 md:col-span-2 row-span-5 md:col-start-4 bg-cv-primary rounded-2xl p-5 order-1 md:order-2">
       <div className="w-full h-full flex items-center justify-center">
         <img
-          src={`${import.meta.env.VITE_BACKEND_SERVER_URL}/photos/${colaborador.user.id}/${
-            colaborador.user.avatar
-          }`}
-          alt=""
+          src={colaborador.user.avatar}
+          alt="img"
           className="w-60 h-60 flex items-center justify-center rounded-full ring ring-cv-cyan object-cover bg-cv-primary"
         />
       </div>
