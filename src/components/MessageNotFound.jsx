22881a51b02a8907b99dd9d5458b0cc1d04171d7
Jefferson <jefferson.silvaquinto@gmail.com>
2024-05-27 11:44:42 -0500

    Modificación en componente MessageNotFound para que pueda mostrar el mensaje ingresado por prop

--- src/components/MessageNotFound.jsx
+++ src/components/MessageNotFound.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 
-const MessageNotFound = () => {
+const MessageNotFound = ({message='No se encontraron resultados'}) => {
     return (
         <div className="w-full gap-7 text-lg py-20 flex flex-col items-center text-center">
             <SearchIcon className='scale-150' fontSize='large'/>
-            <span>No se encontraron resultados</span>
+            <span>{message}</span>
         </div>
     );
 };
