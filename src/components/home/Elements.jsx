d9a1642f8a3aeaa9244a11b1d3e86afbe10fd150
Jefferson <jefferson.silvaquinto@gmail.com>
2024-05-30 21:38:44 -0500

    Mejorando la resolución de la imagen en BirthdayImage que se muestra en la sección inicial de la app

--- src/components/home/Elements.jsx
+++ src/components/home/Elements.jsx
@@ -15,7 +15,7 @@ export const Saludo = () => {
 export const BirthdayImage = ({ item }) => {
 	return (
 		<div className="flex flex-col items-center justify-center gap-4 w-28 md:w-40">
-			{<img src={item.avatar} alt={item.name} className="rounded-full shadow-lg w-28 h-28 md:w-40 md:h-40 ring-2 ring-cv-cyan" />}
+			{<img src={item.avatar} alt={item.name} className="object-cover rounded-full shadow-lg w-28 h-28 md:w-40 md:h-40 ring-2 ring-cv-cyan" />}
 		</div>
 	)
 }
